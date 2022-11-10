da3a50bbc2b7cde9fa10c6c4cf76ca9e28f5e919
MstrJ <jakubkruliczak@gmail.com>
2022-11-10 11:54:00 +0100

    Metoda Get - pobieranie zasobu

--- Blogger/WebAPI/Program.cs
+++ Blogger/WebAPI/Program.cs
@@ -1,15 +1,33 @@
+using Application.Interfaces;
+using Application.Mappings;
+using Application.Services;
+using Domain.Interfaces;
+using Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// 
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddScoped<IPostService, PostService>();
+builder.Services.AddSingleton(AutoMapperConfig.Initialize());
+// ^ dodalem
+
+// pozwala dodac podpowiedzi co dana metoda robi
+builder.Services.AddSwaggerGen(c => { c.EnableAnnotations(); });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
