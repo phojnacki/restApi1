82ed6ff8f6468ff34b973401a58507fa6abb511b
MstrJ <jakubkruliczak@gmail.com>
2022-11-21 15:42:11 +0100

    laczenie sie z mongodb localhost i podlanczone metody

---
+++ Blogger/Application/Services/MongoService.cs
@@ -0,0 +1,62 @@
+﻿using Application.Dto;
+using Application.Interfaces;
+using AutoMapper;
+using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class MongoService : IPostService
+    {
+        private readonly IPostRepository _mongoRepository;
+        private readonly IMapper _mapper;
+
+        // musi byc
+        public MongoService(IPostRepository mongo, IMapper mapper)
+        {
+            _mongoRepository = mongo;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<PostDto>> GetAllPosts()
+        {
+            return _mapper.Map<IEnumerable<PostDto>>(await _mongoRepository.GetAll());
+
+        }
+
+        public async Task<PostDto> GetPostById(int id)
+        {
+            Post post = await _mongoRepository.GetById(id);
+            return _mapper.Map<PostDto>(post);
+        }
+        public PostDto AddNewPost(CreatePostDto newpost)
+        {
+            if (string.IsNullOrEmpty(newpost.Title))
+            {
+                throw new Exception("Post can not have an empty title");
+            }
+            var post = _mapper.Map<Post>(newpost);
+            //post.Id = _
+            _mongoRepository.Add(post);
+            return _mapper.Map<PostDto>(post);
+        }
+
+        public async void UpdatePost(UpdatePostDto updatePost)
+        {
+            var existingPost = await _mongoRepository.GetById(updatePost.Id);
+            var post = _mapper.Map(updatePost, existingPost);
+            _mongoRepository.Update(post);
+        }
+
+        public async void DeletePost(int id)
+        {
+            var post = await _mongoRepository.GetById(id);
+            _mongoRepository.Delete(post);
+        }
+    }
+}
---
+++ Blogger/Infrastructure/Repositories/MongoRepository.cs
@@ -0,0 +1,62 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.VisualBasic;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZstdSharp.Unsafe;
+
+namespace Infrastructure.Repositories
+{
+    public class MongoRepository : IPostRepository
+    {
+        //connect to mongodb
+        private IMongoCollection<Post> _mongoPosts;
+
+
+        public MongoRepository(IMongoClient client)
+        {
+            var database = client.GetDatabase("Blogger");
+            _mongoPosts = database.GetCollection<Post>("Posts");
+        }
+
+
+        public Post Add(Post post)
+        {
+            _mongoPosts.InsertOneAsync(post);
+            return post;
+        }
+
+        public void Delete(Post post)
+        {
+            _mongoPosts.FindOneAndDelete(x => x.Id == post.Id);
+        }
+
+        public async Task<IEnumerable<Post>> GetAll()
+        {
+            var list = await _mongoPosts.Find(_ => true).ToListAsync();
+            return list;
+
+        }
+
+        public async Task<Post> GetById(int id)
+        {
+            var r = await _mongoPosts.FindAsync(x => x.Id == id);
+            return r.ToList().First();
+        }
+
+        public void Update(Post post)
+        {
+            var options = new FindOneAndUpdateOptions<Post> { ReturnDocument = ReturnDocument.Before };
+            var filter = Builders<Post>.Filter.Eq(x => x.Id, post.Id);
+            var update = Builders<Post>.Update.Set(x => x.Content ,post.Content);
+            _mongoPosts.FindOneAndUpdate(filter, update, options);
+            //_mongoPosts.update(filter,update,options);
+        }
+    }
+}
